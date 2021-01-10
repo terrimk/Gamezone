493050230296b6c2ff47f1a551722c6d82ec0bd8
teppuus <tkellerman16@gmail.com>
2021-01-10 17:25:24 -0500

    added stack navigation to About Page

--- routes/aboutStack.js
+++ routes/aboutStack.js
@@ -1,7 +1,7 @@
 /*  
 App Name: GameZone
 Purpose: user can interact with game reviews; practice new features learned about React Native
-File: homestack.js - Contains Navigation stack information, migrating to React Navigation version 5
+File: aboutStack.js - Contains Navigation stack information for the About page, migrating to React Navigation version 5
 Reference: Net Ninja YouTube channel, "React Native Tutorial"
 Date started: 1-5-2021
 For the love of my life
@@ -10,47 +10,38 @@ For the love of my life
 // VER 4: import { createStackNavigator } from 'react-navigation-stack';
 // VER 4: import { createAppContainer } from 'react-navigation';
 // NavigationContainer is react-navigation ver5 - replaces createAppContainer
-import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import Home from '../screens/home';
-import ReviewDetails from '../screens/reviewDetails';
-
+import About from '../screens/about';
 
 /*VER 4
 // screens is an object variable that stores objects that point to the various screen files.
 //        It works by importing the component above and then pointing to it.
 //        The object names in the variable (i.e. Home, ReviewDetails) can be any
 //        name. It just makes sense to call it the same name as the imported Components.
+
 const screens = {
-    Home: {
-        screen: Home,
+    About: {
+        screen: About,
         navigationOptions: {
-             title: 'GameZone',
-             // ADDED DEFAULT BELOW headerStyles: { backgroundColor: '#eee' }
+             title: 'About GameZone',
         }
     },
-    ReviewDetails: {
-        screen: ReviewDetails,
-        navigationOptions: {
-             title: 'Review Details',
-             //ADDED DEFAULT BELOW headerStyles: { backgroundColor: '#eee' }
-        }
-    }
 }
 
 // createStackNavigator is the function imported from react-navigation-stack above
 //    - pass an object into this function that will register which files the 
 //      navigation will point to. In this case, that object is defined above named
 //      'screens'.
-const HomeStack = createStackNavigator(screens, {
+
+const AboutStack = createStackNavigator(screens, {
   defaultNavigationOptions: {
+    headerTintColor: '#444',
     headerStyles: { backgroundColor: '#eee', height: 60 }
   }
 });
 
 // function to pass info about stack to App.js. Stack is ordered top to bottom.
-export default createAppContainer(HomeStack);
+export default AboutStack;
 */
 
 // VER 5
